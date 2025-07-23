2ddcf94083580808d3c013f7059c117417aed912
Trần Thuý Ngân <tranthuyngan2516@gmail.com>
2025-07-23 10:21:07 +0700

    WIP: Thêm các file HoaDon và cập nhật database

---
+++ src/main/java/poly/nhatro/dao/HoaDonDAO.java
@@ -0,0 +1,28 @@
+package poly.nhatro.dao;
+
+import java.util.List;
+import poly.nhatro.entity.HoaDon;
+
+/**
+ *
+ * @author tranthithuyngan
+ */
+public interface HoaDonDAO {
+/**
+     * Lấy tất cả hóa đơn từ database
+     * @return List<HoaDon> danh sách tất cả hóa đơn
+     */
+    List<HoaDon> findAll();
+    
+    List<HoaDon> selectAll();
+    
+    HoaDon selectById(int id);
+    void insert(HoaDon hoaDon);
+    void update(HoaDon hoaDon);
+    void delete(int id);
+    List<HoaDon> selectByTrangThai(boolean daThanhToan);
+    List<Object[]> selectWithDetals();
+            
+    
+    
+};
---
+++ src/main/java/poly/nhatro/dao/impl/hoaDonImpl.java
@@ -0,0 +1,53 @@
+package poly.nhatro.dao.impl;
+
+import java.util.List;
+import poly.nhatro.dao.HoaDonDAO;
+import poly.nhatro.entity.HoaDon;
+
+/**
+ *
+ * @author tranthithuyngan
+ */
+public class hoaDonImpl implements HoaDonDAO {
+
+    @Override
+    public List<HoaDon> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<HoaDon> selectAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public HoaDon selectById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void insert(HoaDon hoaDon) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(HoaDon hoaDon) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<HoaDon> selectByTrangThai(boolean daThanhToan) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Object[]> selectWithDetals() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
