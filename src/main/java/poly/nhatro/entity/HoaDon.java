dde7ddca8d5f6cf637db20477078bc668cc948d6
Trần Thuý Ngân <tranthuyngan2516@gmail.com>
2025-07-31 15:03:52 +0700

    sửa hoá đơn

--- src/main/java/poly/nhatro/entity/HoaDon.java
+++ src/main/java/poly/nhatro/entity/HoaDon.java
@@ -20,6 +20,19 @@ public class HoaDon {
     private int ID_Phong;
     private int ID_HopDong;
     private int ID_ChiNhanh;
-   
-    
+
+    // Các trường được join từ bảng khác
+    private String tenNguoiDung;
+    private String soPhong;
+    private int soDienCu;
+    private int soDienMoi;
+    private int soNuocCu;
+    private int soNuocMoi;
+    private int soDien;
+    private int soNuoc;
+    private double tienDien;
+    private double tienNuoc;
+    private double tienPhong;
+    private double tongTien;
+    private String trangThaiThanhToan;
 }
